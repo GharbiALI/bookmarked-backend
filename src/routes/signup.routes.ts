57444cd89b5b0f2117d28d8096758f939e13c9a6
GharbiALI <gharbialicontacts@gmail.com>
2026-09-01 23:10:13 +0100

    refactor(routes): remove signup.routes.ts (replaced by auth.routes.ts)

--- src/routes/signup.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { signup } from "../controllers/signup.controller";
-import { validateSignupMiddleware } from "../middlewares/signup.middleware";
-
-const router = Router();
-
-router.post("/signup", validateSignupMiddleware, signup);
-
-export default router;
