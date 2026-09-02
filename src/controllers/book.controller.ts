6fdbade36848c544e987592031f13fa55f3ae4f1
GharbiALI <gharbialicontacts@gmail.com>
2026-09-03 00:01:05 +0100

    feat(controller): add getBookHandler

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,6 +1,6 @@
 import { Response } from "express";
 import { AuthRequest } from "../auth/auth.middleware";
-import {listBooks} from "../services/book.service"
+import { listBooks, getBook } from "../services/book.service"
 import { mapBookResponse } from "../mapper/book.mapper";
 
 export const listBooksHandler = async (
@@ -22,6 +22,34 @@ export const listBooksHandler = async (
     return res.status(500).json({
       success: false,
       message: "Failed to fetch books",
+    });
+  }
+};
+
+export const getBookHandler = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<Response> => {
+  try {
+    const book = await getBook(req.params.id);
+
+    if (!book || book.userId.toString() !== req.user!.userId) {
+      return res.status(404).json({
+        success: false,
+        message: "Book not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "Book fetched successfully",
+      data: mapBookResponse(book),
+    });
+
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to fetch book",
     });
   }
 };
