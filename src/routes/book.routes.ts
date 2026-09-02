2249fc42646bce39c34ab272805327827b7762c8
GharbiALI <gharbialicontacts@gmail.com>
2026-09-03 00:01:47 +0100

    feat(routes): add GET /:id route

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { listBooksHandler } from "../controllers/book.controller";
+import { listBooksHandler, getBookHandler } from "../controllers/book.controller";
 import { authenticate } from "../auth/auth.middleware";
+import { validateIdMiddleware } from "../middlewares/book.middleware";
 
 const router = Router();
 
 router.use(authenticate);
 
 router.get("/", listBooksHandler);
+router.get("/:id", validateIdMiddleware, getBookHandler);
 
 export default router;
