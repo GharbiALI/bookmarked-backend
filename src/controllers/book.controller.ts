66ecf7813f298c4406349202618cdb1bd5997199
GharbiALI <gharbialicontacts@gmail.com>
2026-09-03 06:16:08 +0100

    feat(controller): add createBookHandler

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,6 +1,7 @@
+import { Types } from "mongoose";
 import { Response } from "express";
 import { AuthRequest } from "../auth/auth.middleware";
-import { listBooks, getBook } from "../services/book.service"
+import { listBooks, getBook, addBook } from "../services/book.service";
 import { mapBookResponse } from "../mapper/book.mapper";
 
 export const listBooksHandler = async (
@@ -17,7 +18,6 @@ export const listBooksHandler = async (
       message: "Books fetched successfully",
       data: books.map(mapBookResponse),
     });
-
   } catch (err) {
     return res.status(500).json({
       success: false,
@@ -45,11 +45,41 @@ export const getBookHandler = async (
       message: "Book fetched successfully",
       data: mapBookResponse(book),
     });
-
   } catch (err) {
     return res.status(500).json({
       success: false,
       message: "Failed to fetch book",
+    });
+  }
+};
+
+export const createBookHandler = async (
+  req: AuthRequest,
+  res: Response,
+): Promise<Response> => {
+  try {
+    const userId = new Types.ObjectId(req.user!.userId);
+    const { title, author, genre, pages, status, rating } = req.body;
+
+    const book = await addBook({
+      title,
+      author,
+      genre,
+      pages,
+      status,
+      rating,
+      userId,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: "Book created successfully",
+      data: mapBookResponse(book as typeof book & { _id: unknown }),
+    });
+  } catch (err) {
+    return res.status(500).json({
+      success: false,
+      message: "Failed to create book",
     });
   }
 };
