f2d9291f82f37345ffed76a2d026f2831a5c5d54
GharbiALI <gharbialicontacts@gmail.com>
2026-09-03 13:47:14 +0100

    feat(service): add editBook

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,8 +1,15 @@
 import { IBook } from "../schemas/book.schemas";
-import { findBooksByUserId, findBookById, createBook } from "../repository/book.repository";
+import {
+  findBooksByUserId,
+  findBookById,
+  createBook,
+  updateBookById,
+} from "../repository/book.repository";
 
 export const listBooks = async (userId: string) => findBooksByUserId(userId);
 
 export const getBook = async (id: string) => findBookById(id);
 
 export const addBook = async (bookData: Partial<IBook>) => createBook(bookData);
+
+export const editBook = async (id: string, bookData: Partial<IBook>) => updateBookById(id, bookData);
