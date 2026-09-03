6b226d1a63772c5863823cc8fcc0f29d931ba86b
GharbiALI <gharbialicontacts@gmail.com>
2026-09-03 06:15:35 +0100

    feat(middleware): add validateBookMiddleware

--- src/middlewares/book.middleware.ts
+++ src/middlewares/book.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from "express";
-import { validateId } from "../validator/book.validator";
+import { validateId, validateBook } from "../validator/book.validator";
 
 export const validateIdMiddleware = (
   req: Request,
@@ -8,6 +8,25 @@ export const validateIdMiddleware = (
 ) => {
   const { id } = req.params;
   const errors = validateId(id);
+
+  if (errors) {
+    return res.status(400).json({
+      success: false,
+      message: "Validation failed",
+      errors,
+    });
+  }
+
+  next();
+};
+
+export const validateBookMiddleware = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  const { title, author, pages, rating } = req.body;
+  const errors = validateBook(title, author, pages, rating);
 
   if (errors) {
     return res.status(400).json({
